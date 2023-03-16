b31ad1aee5f0156bf5e1c1c13d2172305be1b141
Miguel Fung <64858239+mjfung1@users.noreply.github.com>
2023-03-16 18:32:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+const counterEl = document.querySelector(".counter");
+const loadingBarEl = document.querySelector(".loading-bar-front");
+const btnEl = document.querySelector(".btn-reset");
+
+let idx = 0;
+let timeOut;
+
+function updateNum() {
+    counterEl.innerText = idx + "%";
+    loadingBarEl.style.width = idx + "%";
+    idx++;
+
+    
+    if (idx < 101) {
+        timeOut = setTimeout(updateNum, 40);
+    } else {
+        idx = 0;
+        clearTimeout(timeOut);
+    }
+
+}
+
+
+btnEl.addEventListener("click", () => {
+    idx = 0;
+    clearTimeout(timeOut);
+    updateNum();
+})
